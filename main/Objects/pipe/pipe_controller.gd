d2f19fc98dd2636a464d16e3713666def6f741b3
tjamesw123 <94910672+tjamesw123@users.noreply.github.com>
2022-11-17 20:15:24 -0500

    Modular pipe spawning system with wall gap modifiable + random gap location generated walls

--- main/Objects/pipe/pipe_controller.gd
+++ main/Objects/pipe/pipe_controller.gd
@@ -6,14 +6,13 @@ extends Node2D
 
 var pipeNode = preload("res://Objects/pipe/pipe.tscn")		# preloading the pipe scene from the file tree (not in the node tree) to instance later
 	
+@export var gapRange = Vector2(-60, 60)
 var offset = 0 # the amount the center of the walls meet (normally at the center of the screen) move in pixels
 var bottomLimit = 160 # The size of the y coordinate of the pipe shape used divided by two. Used to find the configration of walls where both the top and bottom walls touch each other with no gape
-var gapDistance = 30 # The distance of the gap between the two walls divided by two
-var generalPosition = bottomLimit + gapDistance # Both the wall zero position and the gap distance divided by two are added together to make the new position
+@export var gapDistance = 30 # The distance of the gap between the two walls divided by two
+var generalPosition = bottomLimit + gapDistance # Both the bottom limit position and the gap distance divided by two are added together to make the new position
 
 func _ready():
-	get_node("pipe_spawner").position = get_node("pipe_controller").position + Vector2(0,-generalPosition+(-offset) ) # Math is done here to calculate the general position plus the offset value to allow the wall to shift up and down
-	get_node("pipe_spawner2").position = get_node("pipe_controller").position + Vector2(0,generalPosition+(-offset) ) # (Negative and postive values are used according to the direction you want to shift)
 	global.game_start.connect(_on_game_started)				# connects to the signal emitted from the global script with the function '_on_game_started'
 
 func _on_game_started():
@@ -22,16 +21,23 @@ func _on_game_started():
 
 func start_pipes():
 	print('pipes starting!')		# check debugger while the game is running.
-	get_node("pipe_spawner").spawn_pipe(pipe_speed)
-	get_node("pipe_spawner2").spawn_pipe(pipe_speed)
+	spawn_pipe(-generalPosition)
+	spawn_pipe(generalPosition)
 	get_node("Timer").start(spawn_speed)		# starts the timer for a time of 'spawn_speed' (set above)
 
 func _on_timer_timeout():			# this is connected using a signal from the child timer Node
 	# when the timer finishes, spawn a new pipe and restart the timer
-	offset = randf_range(-60,60)
-	get_node("pipe_spawner").position = get_node("pipe_controller").position + Vector2(0,-generalPosition+(-offset) ) 
-	get_node("pipe_spawner2").position = get_node("pipe_controller").position + Vector2(0,generalPosition+(-offset) )
-	get_node("pipe_spawner").spawn_pipe(pipe_speed)
-	get_node("pipe_spawner2").spawn_pipe(pipe_speed)
+	offset = randf_range(gapRange.x,gapRange.y)
+	spawn_pipe(-generalPosition)
+	spawn_pipe(generalPosition)
 	get_node("Timer").start(spawn_speed)
 
+func spawn_pipe(generalPosition):
+	# instances new pipe instances using the preloaded var set above
+	var new_pipe = pipeNode.instantiate()	# instances the pipe using the preloaded var
+	new_pipe.move_speed = pipe_speed		# accesses the custom 'move_speed' variable used in the pipe script (check pipe.gd) (it moves using that script)
+	self.add_sibling(new_pipe) # sets the new pipe to be the sibling of the pipe spawner
+	# Math is done here to calculate the general position plus the offset value to allow the wall to shift up and down
+	# (Negative and postive values are used according to the direction you want to shift)
+	new_pipe.position = self.position + Vector2(0,generalPosition+(-offset) ) # sets the position of the new pipe to be equal to the pipe spawner
+	
