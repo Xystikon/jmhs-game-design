e13e591d5c4fe9c31143b222969265749cc7d666
Haroon <haroon.a.amanullah@gmail.com>
2023-01-08 15:50:35 -0500

    moving background

--- main/Levels/level.gd
+++ main/Levels/level.gd
@@ -1,61 +1,62 @@
 extends Node2D
 
+### LEVEL NODE ###
+# - in control of all other nodes
+# - functions to change game state
+# - functions to spawn and kill player
+# - counts pipes passed through to get a score
+
+###
+
+# store various other nodes as variables to use within functions
 @export var pipeController = get_node("pipe_controller")
-var playerPreload = preload("res://Player/Player.tscn")
+@export var background = get_node("background")
+@export var playerPreload = preload("res://Player/Player.tscn")
 @export var player = get_node("Player")
 @export var playerSpawnpoint = get_node("player_spawnpoint")
+
+# set variables
 var pipesSpawned = 1
 var pipesPassedThrough = 0
+
+# init signals
 signal game_started
 signal game_over
 signal game_reset
+
 func addPipesPassedThrough(pipes):
 	pipesPassedThrough += pipes
+
 func addPipesSpawned(pipes):
 	pipesSpawned += pipes
+
 func spawnPlayer():
 	var temp = playerPreload.instantiate()
 	playerSpawnpoint.add_child(temp)
 	player = temp
 	print(player.position)
-	#if player == null:
-		#var temp = playerPreload.instantiate()
-		#playerSpawnpoint.add_child(temp)
-		#player = temp
-		#print(player.position)
-		#player.set_physics_process(false)
-		#player.set_sleeping(true)
-		
-		#player.position = spawnLocation
-		#player.set_pos(spawnLocation)
-		
-		#player.set_physics_process(true)
-		#player.set_sleeping(false)
-		
+
 func killPlayer():
 	if player != null:
 		player.queue_free()
 		game_over.emit()
+
 func startGame():
 	spawnPlayer()
 	game_started.emit()
+
 func gameOver():
 	killPlayer()
+
 func restartGame():
 	killPlayer()
 	resetPipes()
 	startGame()
+	background.reset()
 	game_reset.emit()
+
 func resetPipes():
 	pipeController.reset_pipes()
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	print(pipesPassedThrough)
-	#print(pipesSpawned)
-	#if player != null:
-		#print(player.position)
